9036bcc9113ed36ba77075d231bd4930849d5507
Migrice <efomenakuete@gmail.com>
2021-08-17 17:56:06 +0200

    modification du panel admin

--- disquaire_project/store/admin.py
+++ disquaire_project/store/admin.py
@@ -2,10 +2,39 @@ from django.contrib import admin
 from .models import Artist, Album, Contact, Booking
 
 
-admin.site.register(Album)
-admin.site.register(Artist)
-admin.site.register(Contact)
-admin.site.register(Booking)
+#admin.site.register(Album)
+#admin.site.register(Artist)
 
 
-# Register your models here.
+class BookingInline(admin.TabularInline): #tabularInligne permet d'afficher les informations sur plusieurs lignes
+    model=Booking
+    fieldsets=[
+        (None,{'fields':['album','contacted','created_at',]})
+    ]
+    extra=0 #ne pas afficher de lignes supplementaires
+    verbose_name="Reservation"
+    verbose_name_plural='Reservations'
+
+class AlbumArtistInline(admin.TabularInline):
+    model=Album.artists.through
+    extra=1
+    verbose_name='Disque' #modification du nom de la relation
+    verbose_name_plural='Disques'
+
+
+@admin.register(Contact) #modifier certains parametres dans le panel d'administration
+class ContactAdmin(admin.ModelAdmin):
+    inlines=[BookingInline,] #informations qui s'affichent sur plusieurs lignes avec des colonnes
+
+@admin.register(Artist) #modifier certains parametres dans le panel d'administration
+class ContactArtist(admin.ModelAdmin):
+    inlines=[AlbumArtistInline,] #informations qui s'affichent sur plusieurs lignes avec des colonnes
+
+@admin.register(Album) #modifier certains parametres dans le panel d'administration
+class AlbumAdmin(admin.ModelAdmin):
+    search_fields=['reference','title' ,]
+
+
+@admin.register(Booking) #modifier certains parametres dans le panel d'administration
+class BookingAdmin(admin.ModelAdmin):
+    list_filter=['created_at','contacted' ,]
--- disquaire_project/store/models.py
+++ disquaire_project/store/models.py
@@ -2,39 +2,48 @@ from django.db import models
 
 
 class Artist(models.Model):
-    name = models.CharField(max_length=200, unique=True)
+    name = models.CharField( 'nom' ,max_length=200, unique=True)
 
      #modifier l'affichage dans les classes
     def __str__(self):
         return self.name
+    class Meta:
+        verbose_name='artiste'
 
 class Contact(models.Model):
-    email = models.EmailField(max_length=100)
-    name = models.CharField(max_length=200)
+    email = models.EmailField('email',max_length=100)
+    name = models.CharField('nom',max_length=200)
 
     def __str__(self):
         return self.name
+    class Meta:
+        verbose_name='prospect'
+
 
 
 class Album(models.Model):
-    reference = models.IntegerField(null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    available = models.BooleanField(default=True)
-    title = models.CharField(max_length=200)
-    picture = models.URLField()
+    reference = models.IntegerField('reference',null=True)
+    created_at = models.DateTimeField('date de creation',auto_now_add=True)
+    available = models.BooleanField('disponible',default=True)
+    title = models.CharField('titre',max_length=200)
+    picture = models.URLField('URL de l"image',)
     artists = models.ManyToManyField(Artist, related_name='albums', blank=True)
 
     def __str__(self):
         return self.title
+    
 
 class Booking(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    contacted = models.BooleanField(default=False)
+    created_at = models.DateTimeField('date d"envoie',auto_now_add=True)
+    contacted = models.BooleanField('demande traitée?',default=False)
     album = models.OneToOneField(Album, on_delete=models.CASCADE)
     contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.contact.name
+    class Meta:
+        verbose_name='reservation'
+
 
 
 
