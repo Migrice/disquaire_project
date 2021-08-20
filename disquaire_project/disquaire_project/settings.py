11f5a2029d68b5d6d0a42aa07a41ba1f17bdd26e
Migrice <efomenakuete@gmail.com>
2021-08-20 13:05:58 +0200

    livraison

--- disquaire_project/disquaire_project/settings.py
+++ disquaire_project/disquaire_project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-62-i4(ze#y)b25g_orxj&6d4p1hrtxc05%98&gz^g8e5z4r=&*'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','61@W-F`}e?mzWdBdxJn)>YK>')#'django-insecure-62-i4(ze#y)b25g_orxj&6d4p1hrtxc05%98&gz^g8e5z4r=&*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get('ENV')== 'PRODUCTION':
